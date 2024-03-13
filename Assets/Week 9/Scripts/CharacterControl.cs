bc882d774e87c0194e0fc923b8d2026b7a91c2f9
yangcal <yangcal@OA-S141-09>
2024-03-13 12:46:05 -0400

    Task4b not complete: added function for selecting villager, does not select

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,11 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class CharacterControl : MonoBehaviour
 {
     public static Villager SelectedVillager { get; private set; }
+    public TextMeshProUGUI villagerTypeText;
+
+    void Start()
+    {
+            UpdateSelectedVillager();
+    }
+
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
@@ -13,7 +23,22 @@ public class CharacterControl : MonoBehaviour
             SelectedVillager.Selected(false);
         }
         SelectedVillager = villager;
-        SelectedVillager.Selected(true);
+        if (SelectedVillager != null)
+        {
+            SelectedVillager.Selected(true);
+        }
+        
     }
-    
+
+    private static void UpdateSelectedVillager() {
+        if (SelectedVillager != null)
+        {
+            //villagerTypeText.text = SelectedVillager.GetVillagerType().ToString();
+        }
+        else
+        {
+            //villagerTypeText.text = "No Selection";
+        }
+    }
 }
+   
