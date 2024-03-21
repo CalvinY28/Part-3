1aa65301ce005226c23f7a840fe980f1bd56f75f
CalvinY28 <calvinoct28@gmail.com>
2024-03-20 23:09:47 -0400

    Task 3 Complete: Building effect

--- Assets/Week 10/Scripts/BuildBuilding.cs
+++ Assets/Week 10/Scripts/BuildBuilding.cs
@@ -4,5 +4,38 @@ using UnityEngine;
 
 public class BuildBuilding : MonoBehaviour
 {
-    
+    public GameObject[] buildings;
+    public float scaleDuration = 2;
+
+    private void Start()
+    {
+        StartCoroutine(BuildAllBuildings());
+    }
+
+    IEnumerator BuildAllBuildings() // use build() couritine in here and build the building in the array
+    {
+        foreach (GameObject building in buildings)
+        {
+            yield return StartCoroutine(Build(building));
+        }
+    }
+
+    IEnumerator Build(GameObject building) //refer differnt buildings use in differnt courtouine
+    {
+        float buildTime = 0f;
+        Vector3 initialScale = Vector3.zero; // giving vector3 a name here makes it easier
+        Vector3 targetScale = Vector3.one*5;
+
+        while (buildTime < scaleDuration) // while loop contineus until time reach duration
+        {
+            float t = buildTime / scaleDuration;
+            building.transform.localScale = Vector3.Lerp(initialScale, targetScale, t);
+            buildTime += Time.deltaTime;
+            yield return null;
+        }
+
+        building.transform.localScale = targetScale; // scale is target scale
+    }
 }
+
+
